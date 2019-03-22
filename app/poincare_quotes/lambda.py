4b5c9996a5cca7e5e36be44f1c2af8245ff8fb04
Adam Johnson <me@adamj.eu>
2019-03-22 10:41:54 +0000

    Add first version of deployment

--- app/poincare_quotes/lambda.py
+++
@@ -1,5 +0,0 @@
-from apig_wsgi import make_lambda_handler
-
-from quotes.wsgi import application
-
-lambda_handler = make_lambda_handler(application)
--- app/poincare_quotes/wsgi.py
+++ app/poincare_quotes/wsgi.py
@@ -1,7 +1,10 @@
 import os
 
+from apig_wsgi import make_lambda_handler
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poincare_quotes.settings')
 
 application = get_wsgi_application()
+
+lambda_handler = make_lambda_handler(application)
